0b0ae3db7e931d8e25b1b9ae8881ec440c0e4eb9
EC2 Default User <127326435+mugicomugi@users.noreply.github.com>
2023-06-03 12:38:44 +0000

    12

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,6 +5,10 @@ def new
 end
 
 def confirm
+  @order = Order.new(order_params)
+  @order.postal_code = current_customer.postal_code
+  @order.address = current_customer.address
+  @order.name = current_customer.first_name + current_customer.last_name
 end
 
 def complete
@@ -41,7 +45,7 @@ end
 private
 
 def order_params
-    params.require(:order).permit(:name, :address, :total_price)
+    params.require(:order).permit(:payment_method, :name, :address, :total_price)
 end
 
 end
